3f2e043ec1dea90d4af72fb1e721729d3b484558
Nils <nilsland31@gmail.com>
2020-05-22 23:56:34 +0200

    Correcting movement

--- src/game.cpp
+++ src/game.cpp
@@ -443,7 +443,7 @@ void Game::clickCheck(float mouseX, float mouseY)
         //this->moving_unit = true;
         this->selected_unit->currentPath = aStar(this->tabMap, this->selected_unit->x, this->selected_unit->y, this->lastClickX, this->lastClickY);
         this->selected_unit->isMoving =true;
-        //deplacement(this->selected_unit, lastClickX, lastClickY);
+        deplacement(this->selected_unit, lastClickX, lastClickY);
 
     }
   }
