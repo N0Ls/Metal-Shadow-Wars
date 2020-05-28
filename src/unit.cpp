bd3e5acebb169a5b54df11be42b64d80b692354b
Nils <nilsland31@gmail.com>
2020-05-28 16:19:47 +0200

    Commenting Unit.cpp

--- src/unit.cpp
+++ src/unit.cpp
@@ -10,6 +10,21 @@ using namespace std;
 #include "node.hpp"
 #include "player.hpp"
 
+/**
+ *
+ * Initialize Unit atttributes.
+ *
+ * @param unit The unit to initialize.
+ * @param id The id of the player that owns the unit.
+ * @param owner The reference to the player that owns the unit.
+ * @param pv The number of health points of the unit.
+ * @param force The force of the unit (its fire power).
+ * @param dexterity The dexterity of the unit (its mobility on the map).
+ * @param fireRange The range of the attack of the unit.
+ * @param arrayIndex The index of the unit inside the unit vector of the player.
+ * @param textureId The id of the texture that the unit use
+ *
+ */
 void initUnit(Unit *unit, int id, player *owner, float pv,float force,float dexterity, float fireRange, int arrayIndex, GLuint texture_id){
   unit -> ownerId = id;
   unit -> ownerPlayer = owner;
@@ -24,6 +39,14 @@ void initUnit(Unit *unit, int id, player *owner, float pv,float force,float dext
   unit -> texture_id = texture_id;
 }
 
+
+/**
+ *
+ * Setting the unit atttributes to correspond to a robot.
+ *
+ * @param unit The unit to modify.
+ *
+ */
 void initRobot(Unit *unit){
   unit->pv = 150;
   unit->force = 75;
@@ -33,22 +56,58 @@ void initRobot(Unit *unit){
 }
 
 
+/**
+ *
+ * Set the coordinates of a unit.
+ *
+ * @param unit The unit to modify.
+ * @param x The x coordinate value.
+ * @param y The y coordinate value.
+ *
+ */
 void setCoordinates(Unit *unit, int x, int y){
   unit -> x = x;
   unit -> y = y;
 }
 
+
+/**
+ *
+ * Set the new coordinates of a unit.
+ *
+ * @param unit The unit to modify.
+ * @param x The x coordinate value.
+ * @param y The y coordinate value.
+ *
+ */
+void deplacement(Unit *unit , int destinationX, int destinationY){
+  unit->x = destinationX;
+  unit->y = destinationY;
+}
+
+
+/**
+ *
+ * Function to make the unit move smoothly
+ *
+ * @param unit The unit moving.
+ *
+ */
 void updateDisplayCoordinates(Unit *unit){
 
   float destinationDisplayX;
   float destinationDisplayY;
   float speed = 0.15;
+
+  //checking if the unit movement is finished
   if(!(unit->currentPath.empty())){
+    //Checking if unit is already moving from one tile to another
     if(unit->isMovingToNextTile ==true){
+      //Where the unit is supposed to be
       destinationDisplayX = (-GL_VIEW_SIZE/2)+unit->currentDestination.x*MAP_TILE_SIZE;
       destinationDisplayY = (-GL_VIEW_SIZE/2)+unit->currentDestination.y*MAP_TILE_SIZE;
 
-      //déplacement de l'unité
+      //Moving the unit
       if(unit -> displayX < destinationDisplayX){
         unit ->displayX = unit->displayX + speed;
       }
@@ -63,18 +122,18 @@ void updateDisplayCoordinates(Unit *unit){
         unit->displayY = unit->displayY - speed;
       }
 
-      //on vérifie si on est arrivé sur la case
+      //Checking if we are on the tile
       if(unit->displayX > (destinationDisplayX-0.2) && unit->displayX < (destinationDisplayX+0.2) && unit->displayY >(destinationDisplayY-0.2) && unit->displayY <(destinationDisplayY+0.2)){
-        //cout <<"on est arrivé à la case"<< endl;
-        //on arrondie la position pour être sur une valeur entière
+        //rounding the value so we are on a precise position
         unit->displayX = (-GL_VIEW_SIZE/2)+unit->currentDestination.x*MAP_TILE_SIZE;
         unit->displayY = (-GL_VIEW_SIZE/2)+unit->currentDestination.y*MAP_TILE_SIZE;
 
         unit->isMovingToNextTile = false;
-        unit->currentPath.pop();
+        unit->currentPath.pop(); //removing the current tile
       }
     }
     else{
+      //picking the next destination tile in the stack
       unit->isMovingToNextTile =true;
       unit->currentDestination = unit->currentPath.top();
     }
@@ -87,9 +146,14 @@ void updateDisplayCoordinates(Unit *unit){
 
 }
 
+
+/**
+ *
+ * Function to draw the quads unit
+ *
+ */
 void drawQuadUnit(){
   glBegin(GL_QUADS);
-    //glColor3f(1,1,1);
       glTexCoord2f( 0 , 0);
       glVertex2f(0,0);
 
@@ -105,6 +169,14 @@ void drawQuadUnit(){
 }
 
 
+/**
+ *
+ * Display the unit.
+ *
+ * @param unit The unit to display.
+ * @param textureIds_units[] The array containing the texturesIds
+ *
+ */
 void displayUnit(Unit *unit, GLuint textureIds_units[]){
   glClearColor(0, 0, 0, 0);
   glPushMatrix();
@@ -113,9 +185,11 @@ void displayUnit(Unit *unit, GLuint textureIds_units[]){
   if(unit->isMoving ==true){
     updateDisplayCoordinates(unit);
   }
+  //translating to correct place
   glTranslatef(unit->displayX, unit->displayY,0);
   glBindTexture(GL_TEXTURE_2D, textureIds_units[unit->texture_id]);
-  //glRotatef(90,0,0,1);
+
+  //coloring the unit
   float reduction=0.5;
   if(unit->isDONE==false){
     glColor4f(unit->ownerPlayer->color.r*reduction,unit->ownerPlayer->color.g*reduction,unit->ownerPlayer->color.b*reduction,1);
@@ -124,45 +198,65 @@ void displayUnit(Unit *unit, GLuint textureIds_units[]){
     glColor3f(1,1,1);
   }
   drawQuadUnit();
-
   glPopMatrix();
   glBindTexture(GL_TEXTURE_2D, 0);
 }
 
-void deplacement(Unit *unit , int destinationX, int destinationY){
-  unit->x = destinationX;
-  unit->y = destinationY;
-}
 
+/**
+ *
+ * Print the unit infos in the console.
+ *
+ * @param unit The unit to display infos from.
+ *
+ */
 void printUnitInfos(Unit *unit){
   cout << "Cette unité se situe en"<< unit->x <<", "<< unit->y << '\n';
   cout << "Cette unité utilise la texture"<< unit->texture_id << '\n';
   //cout << "Cette unité appartient à "<< unit->ownerPlayer->name << '\n';
 }
 
+
+/**
+ *
+ * Attack between two units.
+ *
+ * @param attacker The attacking unit
+ * @param defender The defending unit
+ *
+ */
 void attackUnit(Unit *attacker, Unit *defender)
 {
     // When call, attacker correspond to the selected unit,
     // and the defender to the unit corresponding to the position where the click happened.
     int damages = attacker->force * attacker->pv/(float)attacker->pvMax;
-    //int damages = defender->pv
     defender->pv -= damages;
     cout << "Aie j'ai pris " << damages << " dégats dans les dents ! " << endl;
+    //Checking is the defender is dead
     if (defender->pv <= 0)
     {
         defender->isAlive=false;
         std::cout << "Defender is dead." << std::endl;
         player *playerMourning = defender->ownerPlayer;
+        //removing the unit from the player's unit list
         playerMourning->units.erase(playerMourning->units.begin()+defender->arrayIndex);
         playerMourning->nbUnits=playerMourning->units.size();
-        // Retirer l'unité de la liste des unités du joueur
         updateUnitIndex(playerMourning);
     }
 }
 
+/**
+ *
+ * Automatic move for AI units
+ *
+ * @param unit The unit to move.
+ * @param tab[] The array containing all the tiles of the map.
+ * @param unitRef The vector of units in the game
+ *
+ */
 void autoMove(Unit *unit, TileMap tab[],vector<Unit*> &unitRef)
 {
-  //calcul d'une case d'arrivé
+  //chosing a destination tile
   cout << "Unité en : " << unit->x << " : " << unit->y << endl;
   vector <PathCoordinates> possibleTile;
   for(int x=(unit->x-unit->dexterity); x < (unit->x+unit->dexterity); x++){
@@ -193,21 +287,24 @@ void autoMove(Unit *unit, TileMap tab[],vector<Unit*> &unitRef)
   int randIndex = rand() % possibleTile.size();
   PathCoordinates randomTile = possibleTile[randIndex];
 
-
-
   unit->currentPath = aStar(tab, unit->x, unit->y, randomTile.x, randomTile.y);
   unit->isMoving =true;
 
   deplacement(unit, randomTile.x, randomTile.y);
-  // unit->displayX = (-GL_VIEW_SIZE/2)+unit->x*MAP_TILE_SIZE;
-  // unit->displayY = (-GL_VIEW_SIZE/2)+unit->y*MAP_TILE_SIZE;
   unit->hasToAttack =true;
-
-  //unit->isDONE=true;
 }
+
+/**
+ *
+ * Automatic attack for AI units
+ *
+ * @param unit The unit to move.
+ * @param tab[] The array containing all the tiles of the map.
+ * @param unitRef The vector of units in the game
+ *
+ */
 void autoAttack(Unit *unit, TileMap tab[], vector<Unit*> &unitRef){
-  //privielging
-  //checking units
+  //privileging units in range
   for(int w = 0 ; w<(int)unitRef.size(); w++){
     if(abs(unit->x - unitRef[w]->x) + abs(unit->y - unitRef[w]->y) <= unit->fireRange && !(unit->x ==unitRef[w]->x && unit->y ==unitRef[w]->y)){
       attackUnit(unit, unitRef[w]);
