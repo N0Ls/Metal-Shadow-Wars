b421338e3801af30b41e433467071e87d8dc9891
Nils <nilsland31@gmail.com>
2020-05-29 21:13:53 +0200

    Commenting player files

--- include/player.hpp
+++ include/player.hpp
@@ -1,8 +1,6 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-
-
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
 #include <GL/gl.h>
@@ -11,6 +9,8 @@
 #include <vector>
 using namespace std;
 
+
+//Declaring the structure
 struct player {
   int id;
   char name[28];
@@ -21,11 +21,12 @@ struct player {
   SDL_Color color;
 } ;
 
+//Functions related to the player
 void isAlive(player *playerToCheck);
-
 void initPlayer(player *player, int id, int nbUnits, char name[], SDL_Color couleur);
 void updateUnitIndex(player *player);
 
+//Not implemented yet
 void display_infos();
 void buy_unit();
 
--- src/player.cpp
+++ src/player.cpp
@@ -1,29 +1,37 @@
-
 #include <iostream>
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
-
 #include "player.hpp"
 #include "unit.hpp"
 using namespace std;
 
+/**
+ *
+ * Initialize Player's atttributes.
+ *
+ * @param player The player to initialize.
+ * @param id The id of the player.
+ * @param nbUnits The number of units that the player is gooing to get.
+ * @param name The player name.
+ * @param couleur The color of its units.
+ *
+ */
 void initPlayer(player *player, int id, int nbUnits, char name[], SDL_Color couleur)
 {
     player->id = id;
     strcpy(player->name, name);
-    player->color = couleur;  
+    player->color = couleur;
     player->nbUnits = nbUnits;
     player->resources = 45;
     player->isAlive = true;
     std::cout << "Init Units" << '\n';
+
+    //Initialization of the units
     for (int i = 0; i < nbUnits; i++)
     {
       Unit newUnit;
       initUnit(&newUnit, i, player,100, 50, 4, 3, i, 0);
       player->units.push_back(newUnit);
-
-        // setCoordinates(player->units + i, rand()%9, rand()%9);
-        // updateDisplayCoordinates(player->units + i);
     }
     if(player->nbUnits >= 4){
       initRobot(&player->units.front());
@@ -31,6 +39,13 @@ void initPlayer(player *player, int id, int nbUnits, char name[], SDL_Color coul
     }
 }
 
+/**
+ *
+ * Check if the player is alive or not.
+ *
+ * @param player The player to to check.
+ *
+ */
 void isAlive(player *playerToCheck)
 {
   if(playerToCheck->nbUnits <=0){
@@ -38,6 +53,13 @@ void isAlive(player *playerToCheck)
   }
 }
 
+/**
+ *
+ * Update the index number of the units of a player.
+ *
+ * @param player The player to to check.
+ *
+ */
 void updateUnitIndex(player *player){
   for (int i = 0; i < player->units.size(); i++) {
     cout << player->units[i].arrayIndex<<endl;
